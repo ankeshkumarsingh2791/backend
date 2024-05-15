d047d95dc439d5780e9c56680be8478f63181ad1
ankeshkumarsingh2791 <ankeshkumarsingh2791@gmail.com>
2024-05-15 04:23:33 -0700

    error handeling

---
+++ src/app.js
@@ -0,0 +1,25 @@
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+
+const app = express()
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credential: true
+}))
+// form data
+app.use(express.json({
+    limit: "16kb"
+}))
+
+// url data
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+
+// file storage
+app.use(express.static("public"))
+
+app.use(cookieParser())
+
+
+export {app}
